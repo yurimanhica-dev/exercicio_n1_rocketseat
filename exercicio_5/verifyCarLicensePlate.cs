c0440af97af95af049a342a05191b4e804fd8de2
yurimanhica-dev <yurimanhica4@gmail.com>
2026-02-16 15:53:10 +0200

    feat: add github actions

--- exercicio_5/verifyCarLicensePlate.cs
+++ exercicio_5/verifyCarLicensePlate.cs
@@ -7,6 +7,12 @@ public class verifyCarLicensePlate
     {
         Console.WriteLine("Digite a placa do carro:");
         string? licensePlate = Console.ReadLine();
+
+        if (licensePlate == null)
+        {
+            return false;
+        }
+
         bool isValid = Regex.IsMatch(licensePlate, @"^[A-Za-z]{3}[0-9]{4}$");
         
         return isValid;
--- obj/Debug/net9.0/..AssemblyInfo.cs
+++ obj/Debug/net9.0/..AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+878b41bdddbf8e4b78a55983641e7a6637eb6b29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f85d6af86b6969ff1b1a3929c6d9f5fdbde075e3")]
 [assembly: System.Reflection.AssemblyProductAttribute(".")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
